c3be7795c8fab790d2c771a1b02563efa3975439
tkancf <18424095+tkancf@users.noreply.github.com>
2024-02-03 09:03:17 +0900

    Update shell/index.ts file

--- bun/shell/index.ts
+++ bun/shell/index.ts
@@ -1,4 +1,17 @@
-import { $ } from "bun";
+// 外部コマンド実行テスト (問題なく動く)
+// import { $ } from "bun";
+//
+// const repo = "git@github.com:tkancf/tkancf.git";
+// await $`git clone ${repo}`;
 
-const repo = "git@github.com:tkancf/tkancf.git";
-await $`git clone ${repo}`;
+// incremental write file
+// const file = Bun.file("output.txt");
+// const writer = file.writer();
+//
+// writer.write("it was the best of times\n");
+// writer.write("it was the worst of times\n");
+// writer.flush();
+
+// これだと上書きになる
+const data = `It was the best of times, it was the worst of times.`;
+await Bun.write("output.txt", data);
