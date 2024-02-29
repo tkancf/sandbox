c838efdc9d842735af4b07f9ab3ab6345092084a
tkancf <18424095+tkancf@users.noreply.github.com>
2024-02-29 20:47:27 +0900

    タイトルとメタデータを更新しました

--- hono-ssg/src/index.tsx
+++ hono-ssg/src/index.tsx
@@ -7,30 +7,50 @@ import { getPosts } from "./post";
 import { serveStatic } from "@hono/node-server/serve-static";
 
 const app = new Hono();
-const title = "tkancf.com";
+const siteTitle = "tkancf.com";
 const posts = await getPosts();
 type Metadata = {
   title: string;
   pubDate: string;
   description: string;
+  ogImage: string;
 };
 const metadata: Metadata = {
   title: "tkancf.com",
   pubDate: "2024-01-01",
   description: "tkancf.com",
+  ogImage: "/icon.jpg",
 };
 
 const Layout: FC = (props) => {
   return (
     <html class={globalCSS}>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{props.metadata.title}</title>
         <Style />
+        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+        <link rel="sitemap" href="/sitemap.xml" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="title" content={props.metadata.title} />
+        <meta name="description" content={props.metadata.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={props.metadata.url} />
+        <meta property="og:title" content={props.metadata.title} />
+        <meta property="og:description" content={props.metadata.description} />
+        <meta property="og:image" content={props.metadata.ogImage} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={props.metadata.url} />
+        <meta property="twitter:title" content={props.metadata.title} />
+        <meta
+          property="twitter:description"
+          content={props.metadata.description}
+        />
+        <meta property="twitter:image" content={props.metadata.ogImage} />
       </head>
       <body>
         <header>
-          <h1>{title}</h1>
+          <h1>{siteTitle}</h1>
           <nav>
             <a href="/">Home</a>
             <a href="/blog">Blog</a>
