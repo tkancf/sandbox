5385da95a685f925a93a692ba65752f796be342c
tkancf <18424095+tkancf@users.noreply.github.com>
2024-02-29 21:37:57 +0900

    メタデータとTwitterカードを追加

--- hono-ssg/src/Head.tsx
+++ hono-ssg/src/Head.tsx
@@ -10,6 +10,7 @@ export const Head: FC = (props) => {
       <link rel="sitemap" href="/sitemap.xml" />
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
+      /* Add metadata */
       <meta name="title" content={props.metadata.title} />
       <meta name="description" content={props.metadata.description} />
       <meta property="og:type" content="website" />
@@ -17,6 +18,7 @@ export const Head: FC = (props) => {
       <meta property="og:title" content={props.metadata.title} />
       <meta property="og:description" content={props.metadata.description} />
       <meta property="og:image" content={props.metadata.ogImage} />
+      /* Twitter Card */
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={props.metadata.url} />
       <meta property="twitter:title" content={props.metadata.title} />
