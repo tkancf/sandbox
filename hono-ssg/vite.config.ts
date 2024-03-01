6ec27c37dae830b704c693e0ac5517bb6c0f4fd0
tkancf <18424095+tkancf@users.noreply.github.com>
2024-03-01 11:11:36 +0900

    vite-plugin-sitemapを追加し、robots.txtを削除し、vite.config.tsを更新

--- hono-ssg/vite.config.ts
+++ hono-ssg/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import ssg from "@hono/vite-ssg";
 import devServer from "@hono/vite-dev-server";
+import Sitemap from "vite-plugin-sitemap";
 
 const entry = "src/index.tsx";
 
 export default defineConfig(() => {
   return {
-    plugins: [devServer({ entry }), ssg({ entry })],
+    plugins: [
+      devServer({ entry }),
+      ssg({ entry }),
+      Sitemap({ hostname: "https://tkancf.com", generateRobotsTxt: true }),
+    ],
   };
 });
