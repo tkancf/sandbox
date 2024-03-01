4de85750c96cfa76c45c1157a5cca6b27743dc83
tkancf <18424095+tkancf@users.noreply.github.com>
2024-03-01 10:31:44 +0900

    ヘッダーのナビゲーションにアクティブなリンクを追加しました

--- hono-ssg/src/Layout.tsx
+++ hono-ssg/src/Layout.tsx
@@ -4,25 +4,54 @@ import { css } from "hono/css";
 import { Head } from "./Head";
 
 export const Layout: FC = (props) => {
+  const path = new URL(props.metadata.url).pathname;
   return (
     <html class={globalCSS}>
       <Head metadata={props.metadata} />
-      <Header />
+      <Header path={path} />
       <main>{props.children}</main>
       <Footer />
     </html>
   );
 };
 
-export const Header: FC = () => {
+export const Header: FC = (props) => {
+  const headerCSS = css`
+    header {
+      margin: 0em 0 2em;
+    }
+    nav a {
+      margin-right: 1em;
+    }
+    nav a:last-child {
+      margin-right: 0;
+    }
+    h2 {
+      margin: 0.5em 0;
+    }
+    a {
+      display: inline-block;
+      text-decoration: none;
+    }
+    a.active {
+      font-weight: bolder;
+      text-decoration: underline;
+    }
+  `;
   return (
-    <header>
-      <h1>tkancf.com</h1>
+    <header class={headerCSS}>
+      <h2>tkancf.com</h2>
       <nav>
-        <a href="/">Home</a>
-        <a href="/blog">Blog</a>
+        <a className={props.path === "/" ? "active" : ""} href="/">
+          Home
+        </a>
+        <a className={props.path === "/blog" ? "active" : ""} href="/blog">
+          Blog
+        </a>
+        <a className={props.path === "/about" ? "active" : ""} href="/about">
+          About
+        </a>
         <a href="https://github.com/tkancf">GitHub</a>
-        <a href="/about">About</a>
       </nav>
     </header>
   );
